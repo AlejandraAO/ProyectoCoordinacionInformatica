4b9b40ab19da72b309bd121daebef99339799604
Diego Castro <diego.castro.moya@gmail.com>
2016-06-03 16:22:25 -0600

    Se agrego la entidad proyecto , y la logica de negocios de proyecto

---
+++ Entidades/clEntidadProyecto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class clEntidadProyecto
+    {
+        #region Atributos
+        private int idProyecto;
+        private string nombre;
+        private string descripcion;
+        private string tipo;
+        private string estado;
+        private byte informacionProyecto;
+        
+        #endregion
+
+        #region Métodos set y get
+        public int mIdProyecto
+        {
+            set { this.idProyecto = value; }
+            get { return this.idProyecto; }
+        }
+
+        public string mNombre
+        {
+            set { this.nombre = value; }
+            get { return this.nombre; }
+        }
+
+        public string mDescripcion
+        {
+            set { this.descripcion = value; }
+            get { return this.descripcion; }
+        }
+
+        public string mTipo
+        {
+            set { this.tipo = value; }
+            get { return this.tipo; }
+        }
+
+        public string mEstado
+        {
+            set { this.estado = value; }
+            get { return this.estado; }
+        }
+
+       
+
+        public byte mInformacioProyecto
+        {
+            set { this.informacionProyecto = value; }
+            get { return this.informacionProyecto; }
+        }
+
+        #endregion
+    }
+}
---
+++ LogicaNegocios/clProyecto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+using Entidades;
+using System.Data.SqlClient;
+
+namespace LogicaNegocios
+{
+    public class clProyecto
+    {
+
+        //Clase que se encargara de insertar, consultar, modificar y eliminar en la
+        //base de datos de la tabla proyectos.
+        #region Atributos
+        private string strSentencia;
+        #endregion
+
+        #region Metodos
+        public SqlDataReader mConsultarProyectos(clConexion cone)
+        {
+            strSentencia = "select * from tbProyectos";
+            return cone.mSeleccionar(strSentencia, cone);
+        }
+        //public SqlDataReader mConsultarCantidadViajes(clConexion cone, clEntidadProyecto pEntidadProyecto)
+        //{
+        //    strSentencia = "select cantidadPersonas from tbViaje where numViaje='" + pEntidadVIaje.getNumViaje() + "'";
+        //    return cone.mSeleccionar(strSentencia, cone);
+        //}
+
+            //revisar informacion proyecto en la forma que se inserta un varbinary.
+        public Boolean mInsertarProyecto(clConexion cone, clEntidadProyecto pEntidadProyecto)
+        {
+            strSentencia = "Insert into tbProyectos(idProyecto,nombre,descripcion,estado,tipo,informacion)values(" +pEntidadProyecto.mIdProyecto + "'"+pEntidadProyecto.mNombre+"''"+pEntidadProyecto.mDescripcion+"''"+pEntidadProyecto.mEstado+"''"+pEntidadProyecto.mTipo+"'"+pEntidadProyecto.mInformacioProyecto+")";
+            return cone.mEjecutar(strSentencia, cone);
+        }
+
+        //public Boolean mModificarViaje(clConexion cone, clEntidadProyecto pEntidadProyecto)
+        //{
+        //    strSentencia = "Update tbViaje set cantidadPersonas=" + pEntidadViaje.getCantidadPersona() + "  where numViaje= '" + pEntidadViaje.getNumViaje() + "'";
+        //    return cone.mEjecutar(strSentencia, cone);
+        //}
+        #endregion
+    }
+}
