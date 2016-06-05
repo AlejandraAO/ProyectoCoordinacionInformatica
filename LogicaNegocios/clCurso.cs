525d432e7a9bcc21b7f43e03e6f3e731a1225933
Pánfilo Herrera <luis0270@gmail.com>
2016-06-04 18:04:13 -0600

    Se agrega metodo de consultar curso por sigla

--- LogicaNegocios/clCurso.cs
+++ LogicaNegocios/clCurso.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Entidades;
 using AccesoDatos;
+using System.Data.SqlClient;
 
 namespace LogicaNegocios
 {
@@ -17,6 +18,12 @@ namespace LogicaNegocios
         {
             strSentencia = "insert into tbCursos(sigla, nombre, lugar, ciclo, creditos, programa, estado, totalHoras, modalidad) values('"+pEntidadCurso.mSiglaCurso+"', '"+ pEntidadCurso.mNombreCurso+ "', '"+ pEntidadCurso.mLugarCurso+ "', '"+ pEntidadCurso.mCicloCurso+ "', '"+ pEntidadCurso.mCreditosCurso+ "', (SELECT * FROM OPENROWSET(BULK N'" + pEntidadCurso.mProgramaCurso + "', SINGLE_BLOB) as Pdf), '" + pEntidadCurso.mEstadoCurso+ "', '"+ pEntidadCurso.mTotalDeHorasCurso+ "', '"+ pEntidadCurso.mModalidadCurso+ "')      ";
             return conexion.mEjecutar(strSentencia,conexion);
+        }
+
+        public SqlDataReader mConsultaPorSigla(clConexion conexion, clEntidadCurso pEntidadCurso)
+        {
+            strSentencia = "select idCurso, sigla, nombre, lugar, ciclo, creditos, programa, estado, totalHoras, modalidad from tbCursos where sigla='"+pEntidadCurso.mSiglaCurso+"'";
+            return conexion.mSeleccionar(strSentencia, conexion);
         }
     }
 }
