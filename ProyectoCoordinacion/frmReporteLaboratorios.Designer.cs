c6db08c6cdd6419201aa23a7c2132ed0bc408f13
victorCB96 <victor_cb_96@hotmail.com>
2016-06-18 15:47:28 -0600

    Unos cambios

---
+++ ProyectoCoordinacion/frmReporteLaboratorios.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Vista
+{
+    partial class frmReporteLaboratorios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.hora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lunes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.martes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.miercoles = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.jueves = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.viernes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sabado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cbLaboratorio = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.hora,
+            this.lunes,
+            this.martes,
+            this.miercoles,
+            this.jueves,
+            this.viernes,
+            this.sabado});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(743, 269);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // hora
+            // 
+            this.hora.HeaderText = "Hora";
+            this.hora.Name = "hora";
+            this.hora.ReadOnly = true;
+            // 
+            // lunes
+            // 
+            this.lunes.HeaderText = "Lunes";
+            this.lunes.Name = "lunes";
+            this.lunes.ReadOnly = true;
+            // 
+            // martes
+            // 
+            this.martes.HeaderText = "Martes";
+            this.martes.Name = "martes";
+            this.martes.ReadOnly = true;
+            // 
+            // miercoles
+            // 
+            this.miercoles.HeaderText = "Miercoles";
+            this.miercoles.Name = "miercoles";
+            this.miercoles.ReadOnly = true;
+            // 
+            // jueves
+            // 
+            this.jueves.HeaderText = "Jueves";
+            this.jueves.Name = "jueves";
+            this.jueves.ReadOnly = true;
+            // 
+            // viernes
+            // 
+            this.viernes.HeaderText = "Viernes";
+            this.viernes.Name = "viernes";
+            this.viernes.ReadOnly = true;
+            // 
+            // sabado
+            // 
+            this.sabado.HeaderText = "Sabado";
+            this.sabado.Name = "sabado";
+            this.sabado.ReadOnly = true;
+            // 
+            // cbLaboratorio
+            // 
+            this.cbLaboratorio.FormattingEnabled = true;
+            this.cbLaboratorio.Items.AddRange(new object[] {
+            "Lab1",
+            "Lab2"});
+            this.cbLaboratorio.Location = new System.Drawing.Point(131, 33);
+            this.cbLaboratorio.Name = "cbLaboratorio";
+            this.cbLaboratorio.Size = new System.Drawing.Size(121, 21);
+            this.cbLaboratorio.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Laboratorio";
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(679, 33);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 3;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(617, 385);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // frmReporteLaboratorios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ProyectoCoordinacion.Properties.Resources.fondo;
+            this.ClientSize = new System.Drawing.Size(766, 420);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbLaboratorio);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmReporteLaboratorios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "frmReporteLaboratorios";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lunes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn martes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn miercoles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn jueves;
+        private System.Windows.Forms.DataGridViewTextBoxColumn viernes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sabado;
+        private System.Windows.Forms.ComboBox cbLaboratorio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
---
+++ ProyectoCoordinacion/frmReporteProfesores.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using LogicaNegocios;
+using Entidades;
+using AccesoDatos;
+using System.Data.SqlClient;
+
+namespace Vista
+{
+    public partial class frmReporteProfesores : Form
+    {
+        #region Atributos
+        clConexion conexion;
+        menuPrincipal menu;
+        #endregion
+
+
+        public frmReporteProfesores()
+        {
+            
+            InitializeComponent();
+
+        }
+
+        private void dgvReporteProfesor_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+    }
+}
