f82b2e36f60b2d6242febbdb8905f1adf5c39cbd
AlejandraAO <alearroyoo@gmail.com>
2016-06-19 10:19:14 -0600

    cambios frm grupo curso

--- ProyectoCoordinacion/frmGrupoCurso.Designer.cs
+++ ProyectoCoordinacion/frmGrupoCurso.Designer.cs
@@ -62,6 +62,7 @@
             this.btnModificar.TabIndex = 24;
             this.btnModificar.Text = "Modificar";
             this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
             // 
             // btnAgregar
             // 
--- ProyectoCoordinacion/frmGrupoCurso.cs
+++ ProyectoCoordinacion/frmGrupoCurso.cs
@@ -37,6 +37,15 @@ namespace Vista
         private void btnConsultar_Click(object sender, EventArgs e)
         {
             frmConsultarCurso consultaCurso = new frmConsultarCurso(menu);
+            consultaCurso.Show();
+
+           /* if (consultaCurso.get() != "")
+            {
+                usuario.setCodigo(consulta.getCodigo());
+                txtCodigo.Text = consulta.getCodigo();
+                mConsultaCodigo();
+            }*/
+
         }
         
         private void frmGrupoCurso_Load(object sender, EventArgs e)
@@ -75,25 +84,59 @@ namespace Vista
         public void mAgregarGrupo()
         {
             if (mVerificarDatos()== true)
-                {
+               {
+                MessageBox.Show("Se ha insertado el grupo", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
                 clEntidadGrupoCurso.getSetIdCurso = Convert.ToInt32( txtIdGrupo.Text);
                 clEntidadGrupoCurso.getSetNumeroGrup= Convert.ToInt32(txtNumeroGrupo.Text);
                 clEntidadGrupoCurso.getSetCupoMaximo= Convert.ToInt32(txtCupoMaximo.Text);
                 clEntidadGrupoCurso.getSetCupoMinimo= Convert.ToInt32(txtCupoMinimo.Text);
                 clEntidadGrupoCurso.getSetCupoActual= Convert.ToInt32(txtCupoActual.Text);
 
-                if (clGrupoCurso.mInsertarGrupo(conexion, clEntidadGrupoCurso)==true)
-                    {
 
-                    MessageBox.Show("Se ha insertado el grupo", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    mLimpiarCampos();
-                }
+                    if (clGrupoCurso.mInsertarGrupo(conexion, clEntidadGrupoCurso)==true)
+                        {
+
+                        MessageBox.Show("Se ha insertado el grupo", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        mLimpiarCampos();
+                    }
             }
             else
             {
                 MessageBox.Show("No pudo insertar el grupo", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
         }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (mVerificarDatos() == true)
+            {
+
+                conexion.codigo = "123";
+                conexion.clave = "123";
+                clEntidadGrupoCurso.getsetIdGrupo = Convert.ToInt32(txtIdGrupo.Text);
+                clEntidadGrupoCurso.getSetNumeroGrup= Convert.ToInt32(txtNumeroGrupo.Text);
+                clEntidadGrupoCurso.getSetCupoMaximo= Convert.ToInt32(txtCupoMaximo.Text);
+                clEntidadGrupoCurso.getSetCupoMinimo= Convert.ToInt32(txtCupoMinimo.Text);
+                clEntidadGrupoCurso.getSetCupoActual= Convert.ToInt32(txtCupoActual.Text);
+            
+              if (clGrupoCurso.mModificarGrupoCurso(conexion, clEntidadGrupoCurso))
+                {
+                    MessageBox.Show("Se ha modificado el grupo", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("No se ha podido modificar el grupo", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Favor llenar todos los campos", "Datos insuficientes", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+
+        }
+
 
 
     }
