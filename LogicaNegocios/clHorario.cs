77501a7d441c38330454ac4cf2e9b9af71777e61
Pánfilo Herrera <luis0270@gmail.com>
2016-06-21 11:59:30 -0600

    AgregandoHorario

---
+++ LogicaNegocios/clHorario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Entidades;
+using AccesoDatos;
+
+namespace LogicaNegocios
+{
+    public class clHorario
+    {
+        private string sentencia = "";
+
+        public SqlDataReader mConsultarHorarios(clConexion conexion, clEntidadHorario pEntidadHorario)
+        {
+            sentencia = "select c.sigla, h.dia, h.horaInic, h.horaSali from tbHorarios h, tbHorariosGrupCurs hg, tbCursos c, tbGruposCurs gc where h.idHorario = hg.idHorario and c.idCurso = gc.idCurso and gc.idGrupo = hg.idGrupo; ";
+            return conexion.mSeleccionar(sentencia,conexion);
+        }
+    }
+}
