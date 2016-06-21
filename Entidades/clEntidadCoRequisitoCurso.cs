84631fb78b06767013bb5a3a952f85831312f6d4
victorCB96 <victor_cb_96@hotmail.com>
2016-06-21 11:49:32 -0600

    Se agregan las clases de CoRequisitos

---
+++ Entidades/clEntidadCoRequisitoCurso.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class clEntidadCoRequisitoCurso
+    {
+        #region Atributos
+        private int idCurso;
+        private int idCursoCoRequisito;
+        #endregion
+
+        #region Set y Get
+        public int mIdCurso
+        {
+            set { this.idCurso = value; }
+            get { return this.idCurso; }
+        }
+        public int mIdCursoCoRequisito
+        {
+            set { this.idCursoCoRequisito = value; }
+            get { return this.idCursoCoRequisito; }
+        }
+        #endregion
+    }
+}
---
+++ LogicaNegocios/clCoRequisitoCurso.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AccesoDatos;
+using Entidades;
+using System.Data.SqlClient;
+
+namespace LogicaNegocios
+{
+    public class clCoRequisitoCurso
+    {
+        #region Atributos
+        private string sentencia;
+        #endregion
+
+        #region Metodos
+        public Boolean mInsertarCoRequisitoCurso(clConexion conexion,clEntidadCoRequisitoCurso pEntidadCoRequisitoCurso)
+        {
+            sentencia = "insert into tbCorrequisitosCurs(idCurso, idCursoCorre) values (" + pEntidadCoRequisitoCurso.mIdCurso + ", " + pEntidadCoRequisitoCurso.mIdCursoCoRequisito + ")";
+            return conexion.mEjecutar(sentencia, conexion);
+        }
+        #endregion
+
+
+    }
+}
