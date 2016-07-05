5214538aa862c9cfb1502783086aeedfad75a964
victorCB96 <victor_cb_96@hotmail.com>
2016-07-04 21:51:40 -0600

    frmDatosProfesores

---
+++ ProyectoCoordinacion/frmDatosProfesores.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ProyectoCoordinacion
+{
+    partial class frmDatosProfesores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvProfesor = new System.Windows.Forms.DataGridView();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.idProfesor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProfesor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvProfesor
+            // 
+            this.dgvProfesor.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvProfesor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProfesor.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idProfesor,
+            this.nombre,
+            this.apellido1,
+            this.apellido2});
+            this.dgvProfesor.Location = new System.Drawing.Point(12, 27);
+            this.dgvProfesor.Name = "dgvProfesor";
+            this.dgvProfesor.Size = new System.Drawing.Size(766, 296);
+            this.dgvProfesor.TabIndex = 0;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(681, 355);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(97, 38);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // idProfesor
+            // 
+            this.idProfesor.HeaderText = "ID";
+            this.idProfesor.Name = "idProfesor";
+            this.idProfesor.Visible = false;
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            // 
+            // apellido1
+            // 
+            this.apellido1.HeaderText = "Primer Apellido";
+            this.apellido1.Name = "apellido1";
+            // 
+            // apellido2
+            // 
+            this.apellido2.HeaderText = "Segundo Apellido";
+            this.apellido2.Name = "apellido2";
+            // 
+            // frmDatosProfesores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ProyectoCoordinacion.Properties.Resources.fondo;
+            this.ClientSize = new System.Drawing.Size(790, 405);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.dgvProfesor);
+            this.Name = "frmDatosProfesores";
+            this.Text = "frmDatosProfesores";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProfesor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvProfesor;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idProfesor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido2;
+    }
+}
---
+++ ProyectoCoordinacion/frmDatosProfesores.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using LogicaNegocios;
+using Entidades;
+using AccesoDatos;
+using System.Data.SqlClient;
+
+namespace ProyectoCoordinacion
+{
+    public partial class frmDatosProfesores : Form
+    {
+        #region Atributos
+        private SqlDataReader dtrProfesor;
+        private clConexion conexion;
+        private clHorario clHorario;
+        #endregion
+
+
+        public frmDatosProfesores()
+        {
+            conexion = new clConexion();
+            clHorario = new clHorario();
+
+            InitializeComponent();
+            llenarDataGridProfesores();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        public void llenarDataGridProfesores()
+        {
+            dtrProfesor = clHorario.mConsultarProfesores(conexion);
+            if (dtrProfesor != null)
+            {
+                if (dtrProfesor.Read())
+                {
+                    int renglon = dgvProfesor.Rows.Add();
+                    dgvProfesor.Rows[renglon].Cells["idProfesor"].Value = Convert.ToString(dtrProfesor.GetInt32(0));
+                    dgvProfesor.Rows[renglon].Cells["nombre"].Value = dtrProfesor.GetString(1);
+                    dgvProfesor.Rows[renglon].Cells["apellido1"].Value = dtrProfesor.GetString(2);
+                    dgvProfesor.Rows[renglon].Cells["apellido2"].Value = dtrProfesor.GetString(3);
+                }//fin del read
+            }
+        }//fin de llenarData
+
+    }
+}
